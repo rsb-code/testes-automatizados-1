7af030aeca3deb87d1e619922e8d5eef72c7800f
Rutyelle dos Santos Brito <rutyelle@greenlegis.com.br>
2022-07-01 20:12:23 -0300

    Resolução exerc 2 - 01/07

---
+++ helper-api/specs/units/services/SortService.spec.ts
@@ -0,0 +1,37 @@
+import { SortService, TipoOrdenacao } from "../../../src/services/SortService"
+
+describe('Realizando caso de teste na classe SortService', () => {
+  let _service: SortService
+
+  it('Deveria construir corretamenta a classe SortService', () => {
+    _service = new SortService()
+  })
+
+  it ('O método getSort deveria me retornar ["a", "b", "c"], quando lhe for passado ["c", "a", "b"]', () => {
+    // Arrange
+    const palavras = ["a", "b", "c"]
+    const removerDuplicados = false
+    const ordenacao = TipoOrdenacao.Asc
+    const resultadoEsperado = ["c", "a", "b"]
+
+    // Act
+    const result = _service.getSort(palavras, removerDuplicados, ordenacao)
+
+    // Assert
+    expect(result).toStrictEqual(resultadoEsperado)
+  })
+
+  it ('O método getSort não deveria estourar erro, quando a ordenação for undefined e deveria me retornar as palavras sem alteração', () => {
+    // Arrange
+    const palavras = ["a", "b", "c"]
+    const removerDuplicados = false
+    const ordenacao = undefined
+
+    // Act
+    const result = _service.getSort(palavras, removerDuplicados, ordenacao)
+
+    // Assert
+    expect(result).not.toThrowError()
+    expect(result).toStrictEqual(palavras)
+  })
+})
--- helper-api/src/apps/api/routes/index.ts
+++ helper-api/src/apps/api/routes/index.ts
@@ -1,3 +1,4 @@
 import mathRouter from "./MathRoute";
+import sortRouter from "./SortRoute";
 
-export default [mathRouter];
+export default [mathRouter, sortRouter];
